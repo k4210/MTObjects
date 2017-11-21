1d70e196b8c23fe1ca4913a5a9abe6de24d7edd3
maciek.mroz <maciek.mroz@gmail.com>
2017-11-21 20:00:33 +0100

    Reduce size of SmartStack

--- MTObjects/Utils.h
+++ MTObjects/Utils.h
@@ -23,6 +23,8 @@ namespace MTObjects
 {
 	using std::vector;
 
+	typedef unsigned short TIndex;
+
 #ifdef TEST_STUFF 
 	struct TestStuff
 	{
@@ -35,11 +37,10 @@ namespace MTObjects
 
 	namespace SmartStackStuff
 	{
-		static const constexpr unsigned int kCacheLineSize = 64; // == 16 * sizeof(int) == 8 * sizeof(*int)
-		static const constexpr unsigned int kCacheLinesPerChunk = 8;
+		static const constexpr TIndex kDataChunkSize = 64 * 8; // == 16 * sizeof(int) == 8 * sizeof(*int)
 		struct DataChunk
 		{
-			static const constexpr unsigned int kStoragePerChunk = (kCacheLinesPerChunk * kCacheLineSize) - (2 * sizeof(DataChunk*));
+			static const constexpr unsigned int kStoragePerChunk = kDataChunkSize - (2 * sizeof(DataChunk*));
 
 			DataChunk* previous_chunk_ = nullptr;
 			DataChunk* next_chunk_ = nullptr;
@@ -59,7 +60,7 @@ namespace MTObjects
 				return memory_;
 			}
 		};
-		static_assert(sizeof(DataChunk) == (kCacheLineSize * kCacheLinesPerChunk));
+		static_assert(sizeof(DataChunk) == kDataChunkSize);
 
 		struct DataChunkMemoryPool64
 		{
@@ -86,41 +87,41 @@ namespace MTObjects
 					bs_[i] = value;
 				}
 
-				unsigned int FirstZeroIndex() const
+				TIndex FirstZeroIndex() const
 				{
 					unsigned long result = 0;
 					const bool ok = FirstZeroInBitset(bs_, result);
 					Assert(ok);
-					return result;
+					return static_cast<TIndex>(result);
 				}
-/*
+				/*
 
-				std::array<std::bitset<kBitsetSize>, 2> bitsets_;
+								std::array<std::bitset<kBitsetSize>, 2> bitsets_;
 
-				unsigned int FirstZeroIndex() const
-				{
-					unsigned long result1 = 0;
-					const bool ok1 = FirstZeroInBitset(bitsets_[0], result1);
+								unsigned int FirstZeroIndex() const
+								{
+									unsigned long result1 = 0;
+									const bool ok1 = FirstZeroInBitset(bitsets_[0], result1);
 
-					unsigned long result2 = 0;
-					const bool ok2 = FirstZeroInBitset(bitsets_[1], result2);
-					result2 += kBitsetSize;
+									unsigned long result2 = 0;
+									const bool ok2 = FirstZeroInBitset(bitsets_[1], result2);
+									result2 += kBitsetSize;
 
-					Assert(ok1 || ok2);
+									Assert(ok1 || ok2);
 
-					return ok1 ? result1 : result2;
-				}
+									return ok1 ? result1 : result2;
+								}
 
-				bool Test(unsigned int i) const
-				{
-					return bitsets_[i / kBitsetSize][i % kBitsetSize];
-				}
+								bool Test(unsigned int i) const
+								{
+									return bitsets_[i / kBitsetSize][i % kBitsetSize];
+								}
 
-				void Set(unsigned int i, bool value)
-				{
-					bitsets_[i / kBitsetSize][i % kBitsetSize] = value;
-				}
-				*/
+								void Set(unsigned int i, bool value)
+								{
+									bitsets_[i / kBitsetSize][i % kBitsetSize] = value;
+								}
+								*/
 			};
 
 		private:
@@ -138,15 +139,15 @@ namespace MTObjects
 			DataChunkMemoryPool64(DataChunkMemoryPool64&) = delete;
 			DataChunkMemoryPool64& operator=(DataChunkMemoryPool64&) = delete;
 
-			static unsigned int FirstZeroInBitset(const std::bitset<kBitsetSize>& bitset)
+			static TIndex FirstZeroInBitset(const std::bitset<kBitsetSize>& bitset)
 			{
 				unsigned long result = 0xFFFFFFFF;
 				const bool ok = ExtendedBitset::FirstZeroInBitset(bitset, result);
 				Assert(ok);
-				return result;
+				return static_cast<TIndex>(result);
 			}
 
-			DataChunk* Allocate()
+			TIndex Allocate()
 			{
 				const auto first_range_with_free_space = is_range_occupied_.FirstZeroIndex();
 				Assert(first_range_with_free_space < kRangeNum);
@@ -165,12 +166,11 @@ namespace MTObjects
 				TestStuff::max_num_data_chunks_used = std::max<unsigned int>(TestStuff::max_num_data_chunks_used, num_chunks_allocated);
 #endif //TEST_STUFF
 
-				return &chunks_[first_range_with_free_space * kBitsetSize + bit_idx];
+				return first_range_with_free_space * kBitsetSize + bit_idx;
 			}
 
-			void Release(DataChunk* chunk)
+			void Release(TIndex index)
 			{
-				const unsigned int index = static_cast<unsigned int>(std::distance(chunks_.data(), chunk));
 				Assert(index >= 0 && index < kNumberChunks);
 
 				const auto range = index / kBitsetSize;
@@ -186,6 +186,17 @@ namespace MTObjects
 				num_chunks_allocated--;
 #endif //TEST_STUFF
 			}
+
+			DataChunk* GetChunk(TIndex index)
+			{
+				return &chunks_[index];
+			}
+
+			TIndex GetIndex(const DataChunk* chunk) const
+			{
+				return static_cast<TIndex>(std::distance(&chunks_[0], chunk));
+			}
+
 		};
 	};
 
@@ -198,34 +209,48 @@ namespace MTObjects
 	struct SmartStack
 	{
 		static_assert(SmartStackStuff::DataChunk::kStoragePerChunk >= sizeof(T), "too big T");
-		static const constexpr int kElementsPerChunk = SmartStackStuff::DataChunk::kStoragePerChunk / sizeof(T);
+		static const constexpr unsigned int kElementsPerChunk = SmartStackStuff::DataChunk::kStoragePerChunk / sizeof(T);
 
-		SmartStackStuff::DataChunk* first_chunk_ = nullptr;
-		SmartStackStuff::DataChunk* last_chunk_ = nullptr;
-		int number_chunks_ = 0;
-		int number_of_elements_in_last_chunk_ = kElementsPerChunk;
+		static const constexpr TIndex kNullIndex= 0xFFFF;
+
+		TIndex first_chunk_ = kNullIndex;
+		TIndex last_chunk_ = kNullIndex;
+		unsigned short number_chunks_ = 0;
+		unsigned short number_of_elements_in_last_chunk_ = kElementsPerChunk;
 
 	private:
+		static SmartStackStuff::DataChunk* GetPtr(TIndex index)
+		{
+			return (kNullIndex != index) ? SmartStackStuff::DataChunkMemoryPool64::instance.GetChunk(index) : nullptr;
+		}
+
+		static TIndex GetIndex(SmartStackStuff::DataChunk* chunk)
+		{
+			return (nullptr != chunk) ? SmartStackStuff::DataChunkMemoryPool64::instance.GetIndex(chunk) : kNullIndex;
+		}
+
 		void AllocateNextChunk()
 		{
 			auto new_chunk = SmartStackStuff::DataChunkMemoryPool64::instance.Allocate();
-			Assert(new_chunk);
-			new_chunk->Clear();
+			Assert(new_chunk != kNullIndex);
+			auto new_chunk_ptr = GetPtr(new_chunk);
+			new_chunk_ptr->Clear();
 
 			number_chunks_++;
 			Assert(kElementsPerChunk == number_of_elements_in_last_chunk_);
 			number_of_elements_in_last_chunk_ = 0;
 
-			if (last_chunk_)
+			if (kNullIndex != last_chunk_)
 			{
-				Assert(nullptr == last_chunk_->next_chunk_);
-				last_chunk_->next_chunk_ = new_chunk;
-				new_chunk->previous_chunk_ = last_chunk_;
+				auto last_chunk_ptr = GetPtr(last_chunk_);
+				Assert(nullptr == last_chunk_ptr->next_chunk_);
+				last_chunk_ptr->next_chunk_ = new_chunk_ptr;
+				new_chunk_ptr->previous_chunk_ = last_chunk_ptr;
 				last_chunk_ = new_chunk;
 			}
 			else
 			{
-				Assert(nullptr == first_chunk_);
+				Assert(kNullIndex == first_chunk_);
 				last_chunk_ = first_chunk_ = new_chunk;
 			}
 		}
@@ -233,30 +258,30 @@ namespace MTObjects
 		void ReleaseLastChunk()
 		{
 			auto chunk_to_release = last_chunk_;
-			Assert(chunk_to_release);
+			Assert(kNullIndex != chunk_to_release);
 
 			number_chunks_--;
 			Assert(0 == number_of_elements_in_last_chunk_);
 			number_of_elements_in_last_chunk_ = kElementsPerChunk;
 			SmartStackStuff::DataChunkMemoryPool64::instance.Release(chunk_to_release);
 
-			last_chunk_ = chunk_to_release->previous_chunk_;
-			if (last_chunk_)
+			last_chunk_ = GetIndex(GetPtr(chunk_to_release)->previous_chunk_);
+			if (kNullIndex != last_chunk_)
 			{
-				last_chunk_->next_chunk_ = nullptr;
+				GetPtr(last_chunk_)->next_chunk_ = nullptr;
 			}
 			else
 			{
 				Assert(chunk_to_release == first_chunk_);
 				Assert(0 == number_chunks_);
-				first_chunk_ = nullptr;
+				first_chunk_ = kNullIndex;
 			}
 		}
 
 		T* ElementsInLastChunk() const
 		{
-			Assert(last_chunk_);
-			return reinterpret_cast<T*>(last_chunk_->GetMemory());
+			Assert(kNullIndex != last_chunk_);
+			return reinterpret_cast<T*>(SmartStackStuff::DataChunkMemoryPool64::instance.GetChunk(last_chunk_)->GetMemory());
 		}
 	public:
 		unsigned int size() const
@@ -308,14 +333,14 @@ namespace MTObjects
 		{
 			if constexpr (std::is_pod<T>::value)
 			{
-				for (auto chunk = first_chunk_; chunk;)
+				for (auto chunk_ptr = GetPtr(first_chunk_); chunk_ptr;)
 				{
-					auto temo_ptr = chunk;
-					chunk = chunk->next_chunk_;
-					SmartStackStuff::DataChunkMemoryPool64::instance.Release(temo_ptr);
+					auto temo_ptr = chunk_ptr;
+					chunk_ptr = chunk_ptr->next_chunk_;
+					SmartStackStuff::DataChunkMemoryPool64::instance.Release(GetIndex(temo_ptr));
 				}
-				first_chunk_ = nullptr;
-				last_chunk_ = nullptr;
+				first_chunk_ = kNullIndex;
+				last_chunk_ = kNullIndex;
 				number_chunks_ = 0;
 				number_of_elements_in_last_chunk_ = kElementsPerChunk;
 			}
@@ -325,8 +350,8 @@ namespace MTObjects
 				{
 					pop_back();
 				}
-				Assert(first_chunk_ == nullptr);
-				Assert(last_chunk_ == nullptr);
+				Assert(first_chunk_ == kNullIndex);
+				Assert(last_chunk_ == kNullIndex);
 				Assert(number_chunks_ == 0);
 				Assert(number_of_elements_in_last_chunk_ == kElementsPerChunk);
 			}
@@ -437,19 +462,19 @@ namespace MTObjects
 
 		Iter begin() const
 		{
-			return Iter(first_chunk_, 0);
+			return Iter(GetPtr(first_chunk_), 0);
 		}
 
 		Iter end() const
 		{
-			Assert(!empty() || first_chunk_ == nullptr);
-			Assert(!empty() || last_chunk_ == nullptr);
+			Assert(!empty() || first_chunk_ == kNullIndex);
+			Assert(!empty() || last_chunk_ == kNullIndex);
 
 			if (empty() || (number_of_elements_in_last_chunk_ == SmartStack::kElementsPerChunk))
 			{
 				return Iter(nullptr, 0);
 			}
-			return Iter(last_chunk_, number_of_elements_in_last_chunk_);
+			return Iter(GetPtr(last_chunk_), number_of_elements_in_last_chunk_);
 		}
 
 	public:
@@ -473,8 +498,8 @@ namespace MTObjects
 			else
 			{
 				// Move some elements from last chunk
-				const int num_free_slots_in_last_chunk_dst = kElementsPerChunk - dst.number_of_elements_in_last_chunk_;
-				const int num_elements_to_move = std::min(num_free_slots_in_last_chunk_dst, src.number_of_elements_in_last_chunk_);
+				const unsigned short num_free_slots_in_last_chunk_dst = kElementsPerChunk - dst.number_of_elements_in_last_chunk_;
+				const auto num_elements_to_move = std::min(num_free_slots_in_last_chunk_dst, src.number_of_elements_in_last_chunk_);
 				if constexpr(std::is_pod<T>::value)
 				{
 					if (num_elements_to_move)
@@ -513,22 +538,22 @@ namespace MTObjects
 				Assert(dst.number_of_elements_in_last_chunk_ == kElementsPerChunk || src.number_of_elements_in_last_chunk_ == kElementsPerChunk);
 				if (dst.number_of_elements_in_last_chunk_ == kElementsPerChunk)
 				{	//src goes last
-					dst.last_chunk_->next_chunk_ = src.first_chunk_;
-					src.first_chunk_->previous_chunk_ = dst.last_chunk_;
+					GetPtr(dst.last_chunk_)->next_chunk_ = GetPtr(src.first_chunk_);
+					GetPtr(src.first_chunk_)->previous_chunk_ = GetPtr(dst.last_chunk_);
 					dst.last_chunk_ = src.last_chunk_;
 
 					dst.number_of_elements_in_last_chunk_ = src.number_of_elements_in_last_chunk_;
 				}
 				else
 				{	//src goes first
-					src.last_chunk_->next_chunk_ = dst.first_chunk_;
-					dst.first_chunk_->previous_chunk_ = src.last_chunk_;
+					GetPtr(src.last_chunk_)->next_chunk_ = GetPtr(dst.first_chunk_);
+					GetPtr(dst.first_chunk_)->previous_chunk_ = GetPtr(src.last_chunk_);
 					dst.first_chunk_ = src.first_chunk_;
 				}
 				dst.number_chunks_ += src.number_chunks_;
 			}
-			src.first_chunk_ = nullptr;
-			src.last_chunk_ = nullptr;
+			src.first_chunk_ = kNullIndex;
+			src.last_chunk_ = kNullIndex;
 			src.number_chunks_ = 0;
 			src.number_of_elements_in_last_chunk_ = kElementsPerChunk;
 		}
